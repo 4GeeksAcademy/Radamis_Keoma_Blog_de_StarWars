dc5b398a15537bc88f658f0da4b1fa692217d297
radamiscunha19-ux <radamis.cunha19@gmail.com>
2026-01-05 13:44:25 +0000

    store

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export function CardP({ img, data }) {
-    const { name, created } = data;
+    const { name, created, uid } = data;
 
     return (
         <div>
@@ -21,7 +21,7 @@ export function CardP({ img, data }) {
                     <span>{created}</span>
                 </div>
                 <div className="but">
-                    <button><Link to={`/demo/:uid`}>Learn More!</Link></button>
+                    <button><Link to={`/demo/${uid}`}>Learn More!</Link></button>
                     <button className="like">♥</button>
                 </div>
             </div>
@@ -31,7 +31,7 @@ export function CardP({ img, data }) {
 
 export function CardG({ img, data }) {
     if (!data) return null;
-    const { name, uid } = data;
+    const { name, uid, eye_color, height, gender, homeworld, films } = data;
 
     return (
         <div className="card1">
@@ -45,14 +45,24 @@ export function CardG({ img, data }) {
                                 "https://starwars-visualguide.com/assets/img/placeholder.jpg";
                         }}
                     />
-                    <p className="card-text">{name}</p>
+                    <div className="datos">
+                    <h4 className="card-text">{name}</h4>
+                    <p>{homeworld}</p></div>
                 </div>
             </div>
             <div className="Links">
-                <Link to={`/demo/${uid}`}>Color de Ojos</Link>
-                <Link to={`/demo/${uid}`}>Altura</Link>
-                <Link to={`/demo/${uid}`}>Massa</Link>
-                <Link to={`/demo/${uid}`}>Gênero</Link>
+                <div className="datos">
+                <h6>Colorde Ojos</h6>
+                <p>{eye_color}</p>
+                </div>
+                <div className="datos">
+                <h6>Altura</h6>
+                <p>{height}</p>
+                </div>
+                <div className="datos">
+                <h6>Genero</h6>
+                <p>{gender}</p>
+                </div>
             </div>
 
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,23 +8,29 @@ import { useEffect, useState } from "react";
 export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
-	const [personaje, setPersonaje] = useState([])
-
-	useEffect(() => {
+	const getPersonajes = async()=> {
 		fetch("https://www.swapi.tech/api/people")
 			.then(res => res.json())
 			.then(responseAPI => {
-				setPersonaje(responseAPI.results);
-				console.log(responseAPI.results);
+				dispatch({
+					type:"add_personajes",
+					payload: responseAPI.results
+				})
+				console.log(store.personajes);
 			})
 			.catch(err => console.error(err));
+	} 
+	
+	useEffect(() => {
+		if (store.personajes.length == 0)
+		{getPersonajes ()}
 	}, []);
 
 	return (
 		<div>
 			<Navbardos />
 			<div className="container d-flex justify-content-center flex-wrap gap-4 mt-4">
-				{personaje.map(p => (
+				{store.personajes.map(p => (
 					<CardP key={p.uid}
 						img={`https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/${p.uid}.jpg`}
 						data={p}
